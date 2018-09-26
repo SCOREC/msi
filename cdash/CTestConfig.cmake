6de80674624729f348754d42a73600e6f7a1b1df
William Tobin <tobinw3@rpi.edu>
2018-09-26 11:16:48 -0400

    minor tweaks to the nightly test and merge to support upstream testing (xgc_scorec now and eventually also m3dc1)

--- cdash/CTestConfig.cmake
+++ cdash/CTestConfig.cmake
@@ -1,6 +1,6 @@
 set(CTEST_PROJECT_NAME "msi")
-set(CTEST_NIGHTLY_START_TIME "17:00:00 EST")
+set(CTEST_NIGHTLY_START_TIME "07:00:00 EST")
 
 set(CTEST_DROP_METHOD "cp")
-set(CTEST_DROP_LOCATION "/fasttmp/wtobin/nightly/cdash")
+set(CTEST_DROP_LOCATION "/fasttmp/wtobin/nightly/cdash/${CTEST_PROJECT_NAME}")
 set(CTEST_DROP_SITE_CDASH FALSE)
